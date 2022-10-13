bc82404e42f493801d62f9c0e319ccbb3e8b4931
Ademuyiwa Joy <joyoluwatobi36@gmail.com>
2022-10-13 16:55:06 +0100

    Init commit

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.get('/api/whoami', (req, res) => {
+  const language = req.headers['accept-language'] 
+  const software = req.headers['user-agent']
+  const ipaddress = req.ip 
+
+  res.json({ipaddress, language, software})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
